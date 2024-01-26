face3539347d43270be7eb47ed3b4a16dc0dc8e8
TEJAS KAWARKHE <tejas.kawarkhe555@gmail.com>
2024-01-26 09:19:13 +0530

    fixed bug

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 var mongoose=require('mongoose')
 
-mongoose.connect('mongodb+srv://kawarkhetejas9:tejas@cluster0.t8pp5kg.mongodb.net/?retryWrites=true&w=majority')
+
+
+mongoose.connect('mongodb+srv://kawarkhetejas9:tejas@cluster0.t8pp5kg.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true, // Enable SSL
+    sslValidate: true, // Validate SSL certificates
+});
 const userSchema=mongoose.Schema({
     email:String,
     name:String
